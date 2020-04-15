b3975a9883782c9862a74096dc168966b833c628
Park-SeungWoo <54872527+Park-SeungWoo@users.noreply.github.com>
2020-04-15 18:26:10 +0900

    Add files via upload

---
+++ Led_out_blink/Led_out_blink.ino
@@ -0,0 +1,18 @@
+#define Led 22
+void setup() {
+pinMode(Led, OUTPUT);
+}
+void loop() {
+digitalWrite(Led, HIGH); 
+//delay(1000);
+//delay(500);
+delay(100);
+//delay(50);
+//delya(10);
+digitalWrite(Led, LOW); 
+//delay(1000);
+//delay(500);
+delay(100);
+//delay(50);
+//delya(10);
+}
